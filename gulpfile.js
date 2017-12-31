c02453562747168ff016c0704c367220b4d940eb
muhammad sayed <muhammadsayed1001@yahoo.com>
2017-12-31 16:56:30 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const image = require('gulp-image');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['app/assets/css/**/*.scss'])
+
+    .pipe(sass({outputStyle: ''}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+      cascade: false
+      }))
+        .pipe(gulp.dest("app/assets/css"))
+        .pipe(browserSync.stream());
+});
+gulp.task('image', function () {
+    return gulp.src('app/assets/img/**')
+        .pipe(image())
+        .pipe(gulp.dest('app/assets/img'));
+});
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+});
+
+gulp.watch(["app/assets/css/**/*.scss"], ['sass']);
+
+gulp.watch("./index.html").on('change', browserSync.reload);
+gulp.watch("app/assets/js/*.js").on('change', browserSync.reload);
+gulp.watch('app/assets/img/**');
+
+});
+
+// Default Task
+gulp.task('default', ['serve', 'image']);
