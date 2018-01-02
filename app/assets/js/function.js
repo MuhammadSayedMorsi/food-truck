699bece586c019aa6bd6b6ebece22784147e5c2d
muhammad sayed <muhammadsayed1001@yahoo.com>
2018-01-02 17:42:20 +0200

    inital commit

---
+++ app/assets/js/function.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+//   $(".owl-carousel").owlCarousel();
+// AutoHeight.Defaults = {
+//     autoHeight: false,
+//     autoHeightClass: 'owl-height'
+// };
+
+  $('.owl-carousel').owlCarousel({
+    items:1,
+    margin:10,
+    autoHeight:true
+});
+
+
+//   $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+});
